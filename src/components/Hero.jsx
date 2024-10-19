ffccfef9af7ac8990742d8bcfd0c369581a7facb
AkshatRaiShrivastava <92225336+AkshatRaiShrivastava@users.noreply.github.com>
2024-10-19 17:03:19 +0530

    rgr

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
               transition={{ duration: 1, delay: 1.2 }}
               src={profile_pic}
               className="rounded"
-              width={300}
+              width={250}
               alt="profile pic"
             />
           </div>
